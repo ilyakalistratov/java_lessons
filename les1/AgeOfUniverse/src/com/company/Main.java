5cc13d5ec2c87ffabf0773ea86b621540381b38a
ilyakalistratov <ilya.kalistratoff@mail.ru>
2020-03-08 22:15:45 +0400

    [1] Age of Universe

---
+++ les1/AgeOfUniverse/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final long ageOfUniverse = 13_000_000_000L;
+	final int ageOfPeople = 100_000;
+	long result = ageOfUniverse / ageOfPeople;
+        System.out.println("result = " + result);
+    }
+}
