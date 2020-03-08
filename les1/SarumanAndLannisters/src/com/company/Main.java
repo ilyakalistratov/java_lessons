20751c7e0a5724397ed2ff4d98da19a1d636ba7f
ilyakalistratov <ilya.kalistratoff@mail.ru>
2020-03-08 21:46:50 +0400

    [1] Saruman And Lannisters

---
+++ les1/SarumanAndLannisters/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final int lannisterInfantrymans = 5000;
+	final int orcsOfSaruman = 2000;
+	int sum = lannisterInfantrymans + orcsOfSaruman;
+        System.out.println("lannisterInfantrymans = " + lannisterInfantrymans);
+        System.out.println("orcsOfSaruman = " + orcsOfSaruman);
+        System.out.println("sum = " + sum);
+    }
+}
