e8f68dd735efdb3899be8478ddacfed69adf4424
ilyakalistratov <ilya.kalistratoff@mail.ru>
2020-03-08 21:39:52 +0400

    [1] Buratino apples

---
+++ les1/BuratinoAndApples/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final int buratinoNumberOfApples = 7;
+	final int karabasBarabasTookApples = 4;
+	int result = buratinoNumberOfApples - karabasBarabasTookApples;
+        System.out.println("buratinoNumberOfApples = " + buratinoNumberOfApples);
+        System.out.println("karabasBarabasTookApples = " + karabasBarabasTookApples);
+        System.out.println("result = " + result);
+    }
+}
